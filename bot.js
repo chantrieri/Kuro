0b693862411d49bad15fb565cb6505fd99e804e7
chantrieri <40681158+chantrieri@users.noreply.github.com>
2018-06-29 14:50:22 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+"ClientId": 459032256569278473,
+  "Token": "NDU5MDMyMjU2NTY5Mjc4NDcz.Dhcl2Q.yPblgDNOuYBHlJvksBBMIZ7RcWo",
+  "OwnerIds": [
+    280936640304906241
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyD5iUVArkchi9yNkMJLXkceS2P2EhQuEwg",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null
+}
+
+client.login(process.env.BOT_TOKEN);
